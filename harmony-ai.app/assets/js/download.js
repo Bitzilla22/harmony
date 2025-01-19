19a444718104e9db7f968157b3a286428a71f987
Bitzilla22 <102140274+Bitzilla22@users.noreply.github.com>
2025-01-19 07:11:14 -0800

    Update download.js

--- harmony-ai.app/assets/js/download.js
+++ harmony-ai.app/assets/js/download.js
@@ -17,7 +17,7 @@ DOWNLOAD_FORM.addEventListener("submit", (event) => {
     const enteredCode = formData.get("code"); // Get the entered Room ID
 
     // Validate the entered Room ID
-    if (enteredCode === "Bzq8399") {
+    if (enteredCode === "BZQ8399") {
         const fileUrl = "./assets/Harmonylauncher.vbs"; // Path to the file in your directory
         downloadFile(fileUrl);
     } else {
